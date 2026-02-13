2a58b4c2507965333c3e165ced7aa7a27a21b3aa
Gomit Saha <gomit.dev@gmail.com>
2026-02-13 10:22:41 +0530

    Change title from 'My Portfolio' to 'Portfolio'

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <div className="bg-black pt-5 text-white/50 font-serif">
-      <div className="text-5xl text-center">My Portfolio</div>
+      <div className="text-5xl text-center">Portfolio</div>
       <div className="navbar flex justify-center gap-5 text-yellow-500 font-mono">
         <a href="#Intro" className="text-center hover:text-yellow-300 scroll-smooth">
           Introduction
