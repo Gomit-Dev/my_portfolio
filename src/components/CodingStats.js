0b92bd288e2c659f24dab28ba27116ce0f0c6641
Gomit Saha <gomit.dev@gmail.com>
2026-03-20 12:59:37 +0530

    Update CodingStats.js

--- src/components/CodingStats.js
+++ src/components/CodingStats.js
@@ -5,7 +5,7 @@ import LeetCodeChart from "@/components/leet_streak";
 
 export default function CodingStats() {
   return (
-    <div className="bg-zinc-950/90 w-full pt-24 px-4 md:px-10">
+    <div className="bg-zinc-950/90 w-full py-24 px-4 md:px-10">
       <h2 className="text-3xl md:text-4xl font-semibold mb-12 border-l-4 border-yellow-400 pl-4 text-white">
         Coding Stats
       </h2>
